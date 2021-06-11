6239a2a5dd52a98d6aa71e03a05937eb0f8f7d6d
andrew <neon.duncan@gmail.com>
2021-06-11 16:15:10 -0500

    set up json server with postman with basic db.json mock up

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,19 @@
-alert("hello!")
+const recipes = [
+  {title:"Fried Rice",
+    shortDescription:"Classic Chinese fried rice with veggies"},
+  {title:"Chicken Soup",
+    shortDescription:"What you need when you're sick"},
+  { title:"Grilled Cheese",
+    shortDescription:"the classic side for tomato soup"}
+  ]
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadRecipeTitles()
+})
+
+const loadRecipeTitles = (data) => {
+
+}
