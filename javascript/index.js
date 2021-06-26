0d1d49d275924797ada5f6720ae505c7848a46e3
andrew <neon.duncan@gmail.com>
2021-06-26 10:13:07 -0500

    refactored using OOP

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,18 @@
 
-// Gobal get node variables
-  const recipeList = () => document.querySelector("#recipe-list")
-  const titleInput = () => document.querySelector("#title-input")
-  const shortDescriptionInput = () => document.querySelector("#short-description-input")
-  const ingredientsInput = () => document.querySelector("#ingredients-input")
-  const instructionsInput = () => document.querySelector("#instructions-input-list")
-  const instructionsInputList = () => document.querySelectorAll("#instructions-input-list textarea")
-  const authorInput = () => document.querySelector("#author-input")
-  const recipeForm = () => document.querySelector("#recipe-form")
-  const addStepButton = () => document.querySelector("#addStepButton")
-  const viewAllRecipes = () => document.querySelector("#view-all")
-  const searchButton = () => document.querySelector("#search-button")
-  const keySearchSelector = () => document.querySelector("#key-search")
-  const searchInput = () => document.querySelector("#search-input")
+
 
 //onLoad functions
 document.addEventListener("DOMContentLoaded", () => {
   attachSearchButtonFunction()
   attachViewAllRecipesFunction()
 
-  displayAllRecipes()
+  Api.displayAllRecipes()
 
   attachRecipeFormSubmit()
   attachAddStepButton()
 })
+
+
 
 const attachAddStepButton = () => {
   addStepButton().addEventListener('click', (e) => {
@@ -34,50 +23,25 @@ const attachAddStepButton = () => {
 }
 
 const attachSearchButtonFunction = () => {
-  searchButton().addEventListener("click", searchRecipes)
+  searchButton().addEventListener("click", Api.searchRecipes)
 }
 
 const attachViewAllRecipesFunction = () => {
-  viewAllRecipes().addEventListener("click", displayAllRecipes)
+  viewAllRecipes().addEventListener("click", Api.displayAllRecipes)
 }
 
 const attachRecipeFormSubmit = () => {
   recipeForm().addEventListener("submit", e => {
     e.preventDefault()
-    createNewRecipe(getValuesFromInputs())
+    Api.createNewRecipe(getValuesFromInputs())
     recipeForm().reset()
   })
 }
 
-//displays one recipe
-const displayRecipe = (recipe) => {
-  const li = document.createElement("li")
-  const title = document.createElement("h4")
-  const olInstructions = document.createElement("ol")
 
-  title.setAttribute("class", "brown-text text-darken-3")
-  title.innerText = recipe.title
 
-  li.appendChild(title)
-  beautifyDisplayedElement(recipe.shortDescription, li, "Description")
-  beautifyDisplayedElement(recipe.ingredients, li, "Ingredients")
-  displayOrderedInstructions(recipe.instructions, olInstructions)
-  li.appendChild(olInstructions)
-  beautifyDisplayedElement(recipe.author, li, "Author")
 
-  recipeList().appendChild(li)
-}
-
-//helper function to organize the instructions to display in an ordered list
-const displayOrderedInstructions = (instructionArray, orderedList) => {
-  instructionArray.forEach(instruction => {
-    const li = document.createElement("li")
-    li.setAttribute("class", "brown-text text-darken-3")
-    li.innerText = instruction
-    orderedList.appendChild(li)
-  })
-}
-
+//helper function
 //helper function to display each part of the recipe
 const beautifyDisplayedElement = (element, li, subheading) => {
   const h4 = document.createElement("h6")
@@ -90,18 +54,8 @@ const beautifyDisplayedElement = (element, li, subheading) => {
   li.appendChild(p)
 }
 
-//displays all recipes calling displayRecipe while looping through data from the response
-const displayAllRecipes = () => {
-  recipeList().innerHTML = ""
-  fetch("http://localhost:3000/recipeBook")
-  .then( response => response.json())
-  .then(data => {
-    data.forEach(element => {
-      displayRecipe(element)
-    });
-  })
-}
 
+//helper function
 //Puts the values from the form into an object to pass to createRecipe in the fetch POST
 const getValuesFromInputs = () => {
   const recipe = {
@@ -117,27 +71,7 @@ const getValuesFromInputs = () => {
   return recipe
 }
 
-//uses fetch to post the data to the server
-const createNewRecipe = (recipe) => {
-  const fetchConfiguration = {
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json",
-      "Accept":"application/json"
-    },
-    body:JSON.stringify(recipe)
-  }
-
-  fetch("http://localhost:3000/recipeBook", fetchConfiguration)
-  .then(response => response.json())
-  .then(data => {
-    recipeList().innerHTML = ""
-    displayRecipe(data)
-  })
-  .catch(error => console.log(error.message))
-} 
-
-
+//helper function
 //adds another text area to instructions so that each step has its own text box.
 const addStep = () => {
   const li = document.createElement("li")
@@ -152,31 +86,6 @@ const addStep = () => {
 
 //returns a function that takes a value as an argument to search the given key for the value.
 
-const searchFunctionBuilder = (key) => {
-  return (value) => {
-    const recipesMatched = []
-    fetch("http://localhost:3000/recipeBook")
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(recipe => {
-        if (recipe[key].toLowerCase().includes(value.toLowerCase())) {
-          recipesMatched.push(recipe)
-        }
-      })
-      if (recipesMatched[0]) {
-      recipeList().innerHTML = ""
-      recipesMatched.forEach(recipe=>displayRecipe(recipe))
-      } else {
-        alert("No recipes matching that criteria were found.")
-      } 
-    })
-  }
-}
 
-//makes the function to append to the search button
-const searchRecipes = (e) => {
-  e.preventDefault()
-  searchFunctionBuilder(keySearchSelector().value)(searchInput().value)
-}
 
 
